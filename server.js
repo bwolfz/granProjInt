188f6aeeb9d571d3161719bc919a2140719a6be6
bwolfz <kildertavora@gmail.com>
2025-12-14 15:25:15 -0300

    remove qualquer rota wildcard incompatível

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
 const app = express();
-app.use(express.json());
 
-// Rotas do backend
 const routes = require('./src/routes');
 
 app.use(express.json());
 app.use('/api', routes);
 
-// Servir o frontend (React buildado)
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "frontend", "build", "index.html")
-  );
-});
-
-//  Porta dinâmica 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Servidor fullstack rodando");
-}); 
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
