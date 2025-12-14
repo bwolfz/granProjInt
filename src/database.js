06d70b16b90e44ccddb36883e6e4d7658b9a5e20
bwolfz <kildertavora@gmail.com>
2025-12-14 15:40:34 -0300

    use sqlite in memory for render

--- src/database.js
+++ src/database.js
@@ -1,10 +1,19 @@
+//const { Sequelize } = require("sequelize");
+//const path = require('path');
+
+//const sequelize = new Sequelize({
+    //dialect: "sqlite",
+    //storage: path.join(__dirname, "..", "database.sqlite"),
+    //logging: false
+//});
+
+//module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
-const path = require('path');
 
 const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: path.join(__dirname, "..", "database.sqlite"),
-    logging: false
+  dialect: "sqlite",
+  storage: ":memory:"
 });
 
 module.exports = sequelize;
